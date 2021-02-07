f85613406bf78ab25b84982bd60a77d1c06ebefe
Andreas Gut <andreasgut@gmx.ch>
2021-02-07 15:36:13 +0100

    TestKlasse wieder auf 2Spieler-Modus gesetzt.

--- src/TestPlay.java
+++ src/TestPlay.java
@@ -1,7 +1,7 @@
 public class TestPlay {
 
     public static void main(String[] args) {
-        Game game = new Game(new Player("Theres"));
+        Game game = new Game(new Player("Theres"), new Player("Max"));
         try {
             game.play();
         } catch (InvalidFieldException e) {
